86ec138344de9469b96cea56d28c0b6ab976393b
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 13:27:51 -0500

    completed

---
+++ Assignments/OOP/question48/B.java
@@ -0,0 +1,10 @@
+package Assignments.OOP.question48;
+
+public abstract class B {
+    public int m1(int x) {
+        System.out.print("printing x: ");
+        return x;
+    }
+
+    public abstract void show();
+}
