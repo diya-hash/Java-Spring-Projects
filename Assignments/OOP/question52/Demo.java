36ef51b88b4569e9015fbafeac5d09742418f484
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 13:33:40 -0500

    completed

---
+++ Assignments/OOP/question52/Demo.java
@@ -0,0 +1,10 @@
+package Assignments.OOP.question52;
+
+import Assignments.OOP.Employee;
+
+public class Demo {
+    public static void main(String[] args) {
+        Employee e = new Employee(101, "sam", 1000);
+        System.out.println("output: " + e);
+    }
+}
