29f19c71a48c0ab6e86b281c92f6bcf6026d5db9
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 13:03:42 -0500

    completed

---
+++ Assignments/Strings/question39/Demo.java
@@ -0,0 +1,15 @@
+package Assignments.Strings.question39;
+
+public class Demo {
+    public static void main(String[] args) {
+        String str = "How are you Sam?";
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'o') {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
