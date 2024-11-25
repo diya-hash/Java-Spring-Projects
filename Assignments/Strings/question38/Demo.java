63e06564b6d460c5a9dbaf1a937aca0a6e3b5090
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 13:02:41 -0500

    completed

---
+++ Assignments/Strings/question38/Demo.java
@@ -0,0 +1,8 @@
+package Assignments.Strings.question38;
+
+public class Demo {
+    public static void main(String[] args) {
+        String str = "Hello Welcome";
+        System.out.println(str.charAt(1));
+    }
+}
