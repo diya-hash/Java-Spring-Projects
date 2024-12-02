483e82e0caa064e32441bcb7a6784e9fcf761b16
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-02 15:33:04 -0500

    completed

---
+++ Assignments/collection/question68/Demo.java
@@ -0,0 +1,28 @@
+package Assignments.collection.question68;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(5);
+        al.add(7);
+        al.add(9);
+        al.add(10);
+        Collections.sort(al, new myComp());
+        System.out.println(al);
+    }
+}
+
+class myComp implements Comparator<Integer> {
+    public int compare(Integer n, Integer o) {
+        if (n > o)
+            return 1;
+        if (n < o)
+            return -1;
+        return 0;
+    }
+}
