848b45aa5d6cdc7799c5a73c6e5cc1fe9520bead
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-02 16:43:46 -0500

    completed

---
+++ Assignments/collection/question70/Demo.java
@@ -0,0 +1,23 @@
+package Assignments.collection.question70;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Demo {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(102, "ram", 1000);
+        Employee e2 = new Employee(103, "shyam", 2000);
+        Employee e3 = new Employee(101, "sam", 3000);
+        Employee e4 = new Employee(104, "sameer", 3000);
+
+        ArrayList<Employee> al = new ArrayList<>();
+        al.add(e1);
+        al.add(e2);
+        al.add(e3);
+        al.add(e4);
+        Collections.sort(al);
+        for (Employee e : al) {
+            System.out.println(e.getId() + "\t" + e.getName());
+        }
+    }
+}
