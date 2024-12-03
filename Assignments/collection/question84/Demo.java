94c4c9bba1e4bf83396e0856ae557d92d11c0162
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-03 14:36:57 -0500

    completed

---
+++ Assignments/collection/question84/Demo.java
@@ -0,0 +1,15 @@
+package Assignments.collection.question84;
+
+import java.util.HashMap;
+
+public class Demo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "volvo");
+        map.put(2, "bmw");
+        map.put(5, "toyota");
+        map.put(3, "mistubishi");
+        map.put(4, "mercedes");
+        System.out.println(map);
+    }
+}
