0b4dfe12ca44087e82e22a11ba5e267f449e2cf4
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-03 13:21:22 -0500

    completed

---
+++ Assignments/collection/question79/Demo.java
@@ -0,0 +1,15 @@
+package Assignments.collection.question79;
+
+import java.util.HashSet;
+
+public class Demo {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        System.out.println(set);
+    }
+
+}
